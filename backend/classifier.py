215acad3597bbdf5631015235c87a8f04061965c
Rafael Vinicius Wierzba <rafaelwierzba@gmail.com>
2025-09-29 08:55:29 -0300

    Arrumando algumas coisas

--- backend/classifier.py
+++ backend/classifier.py
@@ -1,6 +1,8 @@
 from transformers import pipeline
 
 def classificar_email(texto):
-    classificador = pipeline("text-classification", model="distilbert-base-uncased-finetuned-sst-2-english")
-    resultado = classificador(texto)[0]
-    return "Produtivo" if resultado['label'] == 'POSITIVE' else "Improdutivo"
+    def classificar_email(texto):
+        classificador = pipeline("text-classification", model="distilbert-base-uncased-finetuned-sst-2-english")
+        resultado = classificador(texto)[0]
+        return "Produtivo" if resultado['label'] == 'POSITIVE' else "Improdutivo"
+    
--- backend/responder.py
+++ backend/responder.py
@@ -1,6 +1,5 @@
-from transformers import pipeline
-
 def gerar_resposta(texto, categoria):
+    from transformers import pipeline
     gerador = pipeline("text-generation", model="sshleifer/tiny-gpt2")
     prompt = f"Email: {texto}\nCategoria: {categoria}\nResposta:"
     resposta = gerador(prompt, max_length=100, num_return_sequences=1)[0]['generated_text']
